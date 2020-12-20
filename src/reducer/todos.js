6be6ee14dfe1467c74a7e7df54e0722eccdd2f9c
ramshanadeem <rimshanadeem64@gmail.com>
2020-12-21 02:39:29 +0500

    redux todo

---
+++ src/reducer/todos.js
@@ -0,0 +1,17 @@
+const initialState={
+    data:[],
+};
+
+const todos=(state=initialState,action)=>{
+    switch(action.type)
+    {
+        case'ADD_TODO':
+        return{}
+        case'DELETE_TODO':
+        return{}
+        default:
+             return state;
+    }
+
+}
+export default todos;
